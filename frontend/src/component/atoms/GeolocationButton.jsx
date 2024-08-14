5b029bef6e2c9a524a47496f96b996453a8619bd
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-14 13:21:19 +0200

    feat: Ajout de l'atome Geolocation pour la geolocalisation sur la carte du concert

---
+++ frontend/src/component/atoms/GeolocationButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GeolocationButton = ({ onClick }) => {
+  return (
+    <button 
+      onClick={onClick} 
+      className="geolocation-button" 
+      aria-label="Localiser ma position" 
+    >
+      📍
+    </button>
+  );
+};
+
+GeolocationButton.propTypes = {
+  onClick: PropTypes.func.isRequired, 
+};
+
+export default GeolocationButton;
