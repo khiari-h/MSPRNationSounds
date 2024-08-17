ad2d19c132cd5d945f3b1a84a6be12685efa7825
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-17 22:20:30 +0200

    feat:Ajout migration pour la table event_participant

--- backend/database/migrations/2024_08_17_201931_create_event_participant_table.php
+++ backend/database/migrations/2024_08_17_201931_create_event_participant_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventsTable extends Migration
+class CreateEventParticipantTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('event_name'); 
-            $table->enum('event_type', ['concerts', 'artists']); 
+        Schema::create('event_participant', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('participant_id')->constrained('participants')->onDelete('cascade'); 
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade'); 
             $table->timestamps(); 
         });
     }
@@ -28,6 +28,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('event_participant');
     }
 }
