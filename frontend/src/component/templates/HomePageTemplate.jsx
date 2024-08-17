a24c917e8cbe73a468b48261833175577bfcc63d
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-17 16:41:56 +0200

    feat: Ajout du template pour la home page

---
+++ frontend/src/component/templates/HomePageTemplate.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../organisms/Header';
+import Footer from '../organisms/Footer';
+
+
+
+const HomePageTemplate = ({ heroSection, newsAndUpdates, concertsOverview, ProgrammingOverview, ctaBeforeMap, registerForm, practicalInfo, map, ctaAfterMap }) => {
+  return (
+    <div className="bg-global text-concert-text min-h-screen">
+      <Header />
+      <main>
+        {heroSection}
+        <section aria-labelledby="actualites-mises-a-jour">
+  {newsAndUpdates}
+</section>
+<section aria-labelledby="aperçu-concerts">
+  {concertsOverview}
+</section>
+<section aria-labelledby="aperçu-programmation">
+  {ProgrammingOverview}
+</section>
+<section aria-labelledby="cta-avant-carte">
+  {ctaBeforeMap}
+</section>
+<section aria-labelledby="inscriptions">
+  {registerForm}
+</section>
+<section aria-labelledby="infos-pratiques">
+  {practicalInfo}
+</section>
+
+<section aria-labelledby="carte">
+  {map}
+</section>
+<section aria-labelledby="cta-apres-carte">
+  {ctaAfterMap}
+</section>
+
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+HomePageTemplate.propTypes = {
+  heroSection: PropTypes.node.isRequired,
+  newsAndUpdates: PropTypes.node.isRequired,
+  concertsOverview: PropTypes.node.isRequired,
+  ctaBeforeMap: PropTypes.node.isRequired,
+  practicalInfo: PropTypes.node.isRequired,
+  map: PropTypes.node.isRequired,
+  ctaAfterMap: PropTypes.node.isRequired,
+};
+
+export default HomePageTemplate;
