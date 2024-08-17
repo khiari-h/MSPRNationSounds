54be92132cc2a6bb81bae62c98658b8774c95b72
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-17 22:18:59 +0200

    feat:Ajout migration pour la table event

---
+++ backend/database/migrations/2024_08_17_201841_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id(); // Clé primaire auto-incrémentée
+            $table->string('event_name'); // Nom de l'événement
+            $table->enum('event_type', ['concerts', 'artists']); // Type d'événement (concerts ou artistes)
+            $table->timestamps(); // Colonnes created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
