2ab0b45da68c4884b9f357dfedad635c60ad84ab
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-17 17:09:20 +0200

    feat: Ajout pages de gestion d'erreurs en front

---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\WordpressController;
+use App\Http\Controllers\NewsletterController;
+
+// Route pour l'inscription à la newsletter
+Route::post('/newsletter', [NewsletterController::class, 'subscribe']);
+
+
+// Routes pour les ressources News et Workshops
+Route::resources([
+    'news' => NewsController::class,
+
+]);
+
+// Routes pour les appels à l'API WordPress
+Route::prefix('wordpress')->group(function () {
+    Route::get('points-interets', [WordpressController::class, 'getPointsOfInterest']);
+    Route::get('artists_meetings', [WordpressController::class, 'getArtistsMeetings']);
+    Route::get('concerts', [WordpressController::class, 'getConcerts']);
+    Route::get('partners', [WordpressController::class, 'getPartners']);
+    Route::get('media/{mediaId}', [WordpressController::class, 'getMedia']);
+    Route::get('programming-homepage', [WordpressController::class, 'getProgrammingHomepage']);
+    Route::get('concerts-homepage', [WordpressController::class, 'getConcertsHomepage']);
+});
