c901e9e824246038794a29c812b7fdfcf1d890c6
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-17 16:44:15 +0200

    feat: Ajout de la page programmation

---
+++ frontend/src/component/pages/ProgrammingPage.jsx
@@ -0,0 +1,34 @@
+import React, { useState, Suspense, lazy } from 'react';
+import ProgrammingPageTemplate from '../templates/ProgrammingPageTemplate';
+
+// Importer les composants dynamiquement
+const ConcertsProgramming = lazy(() => import('../organisms/ProgrammingOrganisms/ConcertProgramming'));
+const ArtistMeeting = lazy(() => import('../organisms/ProgrammingOrganisms/ArtistMeeting'));
+
+const ProgrammingPage = () => {
+  const [currentSection, setCurrentSection] = useState('concerts');
+
+  const renderSection = () => {
+    switch (currentSection) {
+      case 'concerts':
+        return <ConcertsProgramming />;
+      case 'artistMeetings':
+        return <ArtistMeeting />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <ProgrammingPageTemplate
+      currentSection={currentSection}
+      onSectionChange={setCurrentSection}
+    >
+      <Suspense fallback={<div>Loading...</div>}>
+        {renderSection()}
+      </Suspense>
+    </ProgrammingPageTemplate>
+  );
+};
+
+export default ProgrammingPage;
