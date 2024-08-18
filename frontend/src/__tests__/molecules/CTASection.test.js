c4960bc2bc379bb41a1d2a86df7572a7c7a21072
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-18 10:53:49 +0200

    feat:Mise à jour fichier de test CTASection

--- frontend/src/__tests__/molecules/CTASection.test.js
+++ frontend/src/__tests__/molecules/CTASection.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { act } from 'react';
 import CTASection from '../../component/molecules/CTASection';
 
