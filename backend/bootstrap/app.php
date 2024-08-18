2a45bb048ae852b4906deab0a340353f572b0bb4
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-18 22:01:00 +0200

    feat: Ajout middleware ContentSecurityPolicy

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            
+            ContentSecurityPolicy::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
