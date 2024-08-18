dbeeeca3a723c9df354b0e92bd32d558d84a8b69
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-18 20:26:56 +0200

    feat: Ajout MCD MLD et MPD

--- backend/tests/Unit/EventTest.php
+++ backend/tests/Unit/EventTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Event;
-use App\Models\Participant;
 
 class EventTest extends TestCase
 {
@@ -16,31 +15,12 @@ class EventTest extends TestCase
     {
         $event = Event::create([
             'event_name' => 'Festival',
-            'event_type' => 'Music',
+            'event_type' => 'concerts',  
         ]);
 
         $this->assertDatabaseHas('events', [
             'event_name' => 'Festival',
-            'event_type' => 'Music',
+            'event_type' => 'concerts',  
         ]);
-    }
-
-    /** @test */
-    public function it_can_have_many_participants()
-    {
-        $event = Event::create([
-            'event_name' => 'Festival',
-            'event_type' => 'Music',
-        ]);
-
-        $participant = Participant::create([
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-            'email' => 'john@example.com',
-        ]);
-
-        $event->participants()->attach($participant->id);
-
-        $this->assertTrue($event->participants->contains($participant));
     }
 }
