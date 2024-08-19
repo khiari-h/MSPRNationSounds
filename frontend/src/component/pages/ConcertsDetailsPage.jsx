c410b8818be95d0cec9562c1c30eabc44c3878d2
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-08-19 18:29:01 +0200

    feat: refactorisatio composant ConcertDetailsPage

--- frontend/src/component/pages/ConcertsDetailsPage.jsx
+++ frontend/src/component/pages/ConcertsDetailsPage.jsx
@@ -4,48 +4,30 @@ import ConcertsDetailsPageTemplate from '../templates/ConcertsDetailsPageTemplat
 import InfoCard from '../molecules/InfoCard';
 import Filter from '../atoms/Filter';
 import Button from '../atoms/Button'; 
+import { fetchWithCache } from '../../utils/cacheUtils';
+import { formatDate, formatTime } from '../../utils/formatUtilis';
+import { useResponsiveItemsPerPage } from '../../hooks/useResponsiveItemPerPage';
 
 const ConcertsDetailsPage = () => {
   const [concerts, setConcerts] = useState([]);
   const [filters, setFilters] = useState({ group: '', date: '', lieu: '', type: '' });
   const [currentPage, setCurrentPage] = useState(1);
   const [concertsPerPage, setConcertsPerPage] = useState(6);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true); // Ajout de l'état de chargement
 
-  const formatDate = (dateStr) => {
-    if (!dateStr) return dateStr;
-    const year = dateStr.slice(0, 4);
-    const month = dateStr.slice(4, 6);
-    const day = dateStr.slice(6, 8);
-    return `${day}/${month}/${year}`;
-  };
-
-  const formatTime = (timeStr) => {
-    if (!timeStr) return timeStr;
-    const [hour, minute] = timeStr.split(':');
-    return `${hour}:${minute}`;
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setConcertsPerPage(window.innerWidth < 768 ? 3 : 6);
-    };
-    
-    window.addEventListener('resize', handleResize);
-    handleResize(); // Initial call to set concertsPerPage based on screen size
-    
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // Utiliser le hook pour ajuster le nombre de concerts par page en fonction de la taille de l'écran
+  useResponsiveItemsPerPage(setConcertsPerPage);
 
   useEffect(() => {
     const fetchConcerts = async () => {
       try {
-        const response = await axios.get('/api/wordpress/concerts');
-        const concertsData = response.data;
+        const concertsData = await fetchWithCache('concertsDetails', '/api/wordpress/concerts', 3600);
 
         const concertsWithDetails = await Promise.all(concertsData.map(async concert => {
           if (concert.acf.photo) {
-            const mediaResponse = await axios.get(`https://nationsounds.online/wp-json/wp/v2/media/${concert.acf.photo}`);
-            concert.acf.photo = mediaResponse.data.source_url;
+            const logoData = await fetchWithCache(`logo_${concert.acf.photo}`, `https://nationsounds.online/wp-json/wp/v2/media/${concert.acf.photo}`, 3600);
+            concert.acf.photo = logoData.source_url;
           }
           concert.acf.date = formatDate(concert.acf.date);
           concert.acf.heuredebut = formatTime(concert.acf.heuredebut);
@@ -56,6 +38,9 @@ const ConcertsDetailsPage = () => {
         setConcerts(concertsWithDetails);
       } catch (error) {
         console.error("Erreur lors de la récupération des concerts!", error);
+        setError("Une erreur est survenue lors de la récupération des concerts. Veuillez réessayer plus tard.");
+      } finally {
+        setLoading(false); // Arrêter le chargement une fois que la récupération est terminée
       }
     };
 
@@ -63,7 +48,7 @@ const ConcertsDetailsPage = () => {
   }, []);
 
   const handleFilterChange = (key, value) => {
-    setFilters((prevFilters) => ({ ...prevFilters, [key]: value }));
+    setFilters(prevFilters => ({ ...prevFilters, [key]: value }));
   };
 
   const resetFilters = () => {
@@ -102,28 +87,36 @@ const ConcertsDetailsPage = () => {
       }
       concerts={
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {currentConcerts.map((concert, index) => (
-              <InfoCard
-                key={index}
-                title={concert.acf.nom}
-                description={concert.acf.description}
-                image={concert.acf.photo}
-                additionalInfo={`Date: ${concert.acf.date}, Heure de début: ${concert.acf.heuredebut}, Heure de fin: ${concert.acf.heurefin}, Lieu: ${concert.acf.lieu}, Type: ${concert.acf.type}`}
-                type="program"
-              />
-            ))}
-          </div>
-          <div className="flex justify-center mt-4">
-            {Array.from({ length: totalPages }, (_, index) => (
-              <Button 
-                key={index + 1}
-                onClick={() => setCurrentPage(index + 1)}
-                className="mx-1"
-                label={String(index + 1)}
-              />
-            ))}
-          </div>
+          {loading ? (
+            <p>Chargement des concerts...</p>
+          ) : error ? (
+            <p className="text-red-500">{error}</p>
+          ) : (
+            <>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {currentConcerts.map((concert, index) => (
+                  <InfoCard
+                    key={index}
+                    title={concert.acf.nom}
+                    description={concert.acf.description}
+                    image={concert.acf.photo}
+                    additionalInfo={`Date: ${concert.acf.date}, Heure de début: ${concert.acf.heuredebut}, Heure de fin: ${concert.acf.heurefin}, Lieu: ${concert.acf.lieu}, Type: ${concert.acf.type}`}
+                    type="program"
+                  />
+                ))}
+              </div>
+              <div className="flex justify-center mt-4">
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <Button 
+                    key={index + 1}
+                    onClick={() => setCurrentPage(index + 1)}
+                    className="mx-1"
+                    label={String(index + 1)}
+                  />
+                ))}
+              </div>
+            </>
+          )}
         </>
       }
     />
